b632239bc4a24ccc4615a2d9c4df876e6323aa82
Jorge Hidalgo <jorge.hidalgo@gmail.com>
2023-05-10 11:49:32 +0200

    upgrade value types example to Records

--- value-types/src/main/java/valhallaBench/Complex.java
+++ value-types/src/main/java/valhallaBench/Complex.java
@@ -1,16 +1,6 @@
 package valhallaBench;
 
-public class Complex {
-  private final double re;
-  private final double im;
-
-  public Complex(double re, double im) {
-    this.re = re;
-    this.im = im;
-  }
-
-  public double re() { return re; }
-  public double im() { return im; }
+public record Complex(double re, double im) {
 
   public Complex add(Complex that) {
     return new Complex(this.re + that.re, this.im + that.im);
